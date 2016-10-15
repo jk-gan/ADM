dfead6710157fd77c263350ee4c1d39c88ece292
Gan Jun Kai <kuhn96@gmail.com>
2016-10-16 02:03:50 +0800

    Added containers folder

---
+++ app/containers/input.container.jsx
@@ -0,0 +1,12 @@
+// React library
+import React from 'react';
+
+class InputContainer extends React.Component {
+	constructor(props) {
+    	super(props);
+    }
+
+    // componentDidMount lifecycle method. Called once a component is loaded
+    componentDidMount() {
+    }
+}
